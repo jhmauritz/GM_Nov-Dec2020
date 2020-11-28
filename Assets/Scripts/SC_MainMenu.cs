9ef9196d06a3fb6882351eea58ead665e0a31d89
ChamelK <chamelkmcbride@gmail.com>
2020-11-28 14:22:22 -0500

    Created Main Menu with working buttons, added placeholder text, created Character Selection Screen

---
+++ Assets/Scripts/SC_MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MainMenu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CharacterSelect");
+    }
+
+    public void CreditsButton()
+    {
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
