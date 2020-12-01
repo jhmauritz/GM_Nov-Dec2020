f8814741f5650108aac24806a2c94ee73e8a10c6
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-12-01 09:39:12 -0600

    added a pause into the game to match the beginning match text

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -59,6 +59,10 @@ public class PlayerScript : MonoBehaviour
 
     [Header("Effects")] 
     public GameObject bloodEffects;
+    
+    //timer variables
+    bool startTimer = true;
+    public float timer = 4f;
     #endregion
 
     #region INITIALIZATION
@@ -116,42 +120,64 @@ public class PlayerScript : MonoBehaviour
     
     void Update()
     {
+        #region START_TIMER
+
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                startTimer = false;
+                timer = 4f;
+            }
+        }
+        
+        #endregion
+        
         #region MOVEMENT_FUNCTIONING
-        float moveX = movementInput.x * movementInput.x;
-        animator.SetFloat("Blend", moveX);
-        
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+
+        if (!startTimer)
         {
-            playerVelocity.y = 0f;
+            float moveX = movementInput.x * movementInput.x;
+
+            animator.SetFloat("Blend", moveX);
+
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+
+            Vector3 move = new Vector3(movementInput.x, 0, 0);
+
+            controller.Move(move * playerSpeed * Time.deltaTime);
+
+
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
+
+            // Changes the height position of the player..
+            if (jumped && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            playerVelocity.z = 0f;
+            controller.Move(playerVelocity * Time.deltaTime);
+
+            if (impact.magnitude > 0.2)
+            {
+                controller.Move(impact * Time.deltaTime);
+            }
+
+            impact.z = 0f;
+            impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
         }
 
-        Vector3 move = new Vector3(movementInput.x, 0, 0);
-        controller.Move(move  * playerSpeed * Time.deltaTime);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Changes the height position of the player..
-        if (jumped && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        playerVelocity.z = 0f;
-        controller.Move(playerVelocity * Time.deltaTime);
-
-        if (impact.magnitude > 0.2)
-        {
-            controller.Move(impact * Time.deltaTime);
-        }
-
-        impact.z = 0f;
-        impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
-        
         #endregion
 
         #region UPDATE_DAMAGE_DEALING
