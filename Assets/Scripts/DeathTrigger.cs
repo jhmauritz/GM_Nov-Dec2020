a8563f9ac29b1dd5323ae9e1450e13784f661446
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-12-01 16:56:04 -0600

    constrain on the z and death trigger

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerScript>())
+        {
+            other.gameObject.GetComponent<PlayerScript>().TakeDamage(
+                other.gameObject.GetComponent<PlayerScript>().currHealth);
+        }
+    }
+}
