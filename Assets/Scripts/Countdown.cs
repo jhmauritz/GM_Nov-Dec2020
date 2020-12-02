2fa212bb57696712aa01e31d05b0b1a3ee3586d1
ChamelK <chamelkmcbride@gmail.com>
2020-12-02 17:48:02 -0500

    Created Player Icons and Timer/UI Edits

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+   float timeLeft = 60.0f;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeText.text = (timeLeft).ToString("0");
+        if(timeLeft < 0)
+        {
+            //GameOver();
+        }
+    }
+}
