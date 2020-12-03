1ef30fc6fb3cb60d9be7597a5eaa28675be9dc2e
ChamelK <chamelkmcbride@gmail.com>
2020-12-03 10:27:33 -0500

    Added Character Selection Code

---
+++ Assets/Scripts/C_LoadCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class C_LoadCharacter : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    //public Transform spawnPoint;
+    public Text label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        //GameObject clone = Instantiate(prefab.spawnPoint.position, Quanternion.identity);
+        label.text = prefab.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/C_Selection.cs
+++ Assets/Scripts/C_Selection.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class C_Selection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] characters;
+    public int selectedCharacter = 0;
+
+    public void NextCharacter()
     {
-        
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PreviousCharacter()
     {
-        
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if(selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters.Length[selectedCharacter].SetActive(true);
     }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("prototyping");
+    }
+   
 }
