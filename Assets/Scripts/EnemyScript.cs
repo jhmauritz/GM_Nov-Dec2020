da1176b9d23428165aaa59fe6ec56ec8bf44e265
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2020-12-08 22:05:43 -0600

    working on enemies now

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    //this will hold all of the state machine (switch statements) and navigation for the enemies 
+    //looking into best way to do it
+    
+}
